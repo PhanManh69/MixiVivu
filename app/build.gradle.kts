0d94b47a343263a5b6fcc778c0a6f1c6ae3d32aa
pkmanh <phankhacmanh.6903@gmail.com>
2024-07-13 15:20:59 +0700

    setup build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,12 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.bundles.lifecycleLibs)
+    implementation(libs.bundles.activityLibs)
+    implementation(libs.bundles.comLibs)
 }
