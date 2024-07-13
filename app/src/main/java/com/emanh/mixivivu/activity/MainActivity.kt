39837b42379715cefbb226949159767278167e22
pkmanh <phankhacmanh.6903@gmail.com>
2024-07-13 23:22:32 +0700

    setup bar menu main

--- app/src/main/java/com/emanh/mixivivu/activity/MainActivity.kt
+++ app/src/main/java/com/emanh/mixivivu/activity/MainActivity.kt
@@ -1,21 +1,72 @@
 package com.emanh.mixivivu.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import androidx.core.content.ContextCompat
 import com.emanh.mixivivu.R
+import com.emanh.mixivivu.databinding.ActivityMainBinding
+import com.emanh.mixivivu.databinding.ButtonBarItemBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init() {
+        val bottomBarItems = listOf(
+            BottomBarItem(binding.buttonShip, R.drawable.ship, "Du Thuyền", 0),
+            BottomBarItem(binding.buttonPlane, R.drawable.plane, "Vé máy bay", 1),
+            BottomBarItem(binding.buttonBlog, R.drawable.blog, "Blog", 2),
+            BottomBarItem(binding.buttonEnterprise, R.drawable.enterprise, "Giới thiệu", 3)
+        )
+
+        var selectedPosition = 0
+
+        bottomBarItems.forEachIndexed { index, item ->
+            setupButtonBarItem(item.binding, item.iconRes, item.text, selectedPosition)
+            item.binding.root.setOnClickListener {
+                selectedPosition = index
+                bottomBarItems.forEach { otherItem ->
+                    setupButtonBarItem(otherItem.binding, otherItem.iconRes, otherItem.text, selectedPosition)
+                }
+            }
         }
     }
+
+    private fun setupButtonBarItem(itemBinding: ButtonBarItemBinding, iconRes: Int, text: String, selectedPosition: Int) {
+        val position = when (itemBinding) {
+            binding.buttonShip -> 0
+            binding.buttonPlane -> 1
+            binding.buttonBlog -> 2
+            binding.buttonEnterprise -> 3
+            else -> -1
+        }
+
+        val isSelected = position == selectedPosition
+        val colors = intArrayOf(
+            ContextCompat.getColor(this, R.color.grey),
+            ContextCompat.getColor(this, R.color.darkBlue)
+        )
+
+        itemBinding.apply {
+            icon.setImageResource(iconRes)
+            icon.setColorFilter(colors[if (isSelected) 1 else 0])
+            title.text = text
+            title.setTextColor(colors[if (isSelected) 1 else 0])
+            indicator.visibility = if (isSelected) View.VISIBLE else View.INVISIBLE
+        }
+    }
+
+    private data class BottomBarItem(
+        val binding: ButtonBarItemBinding,
+        val iconRes: Int,
+        val text: String,
+        val position: Int
+    )
 }
